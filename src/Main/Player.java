39fdda20c3041398e1559d8c45a350311f9c1850
meiskiet <113913528+meiskiet@users.noreply.github.com>
2022-11-17 22:49:43 +0700

    Update Player.java

--- src/Main/Player.java
+++ src/Main/Player.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Player {
     private int Galy;
     private int Mana;
+    private String name;
 
     public Player(){
         // Default galy and mana values
