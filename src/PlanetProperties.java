826ed44ab3a00591bc9693c7b972fd0fd1a4aaef
itzmealvin <86088186+itzmealvin@users.noreply.github.com>
2022-12-28 18:51:23 +0700

    Update PlanetProperties.java

--- src/PlanetProperties.java
+++ src/PlanetProperties.java
@@ -2,18 +2,18 @@
 import java.util.ArrayList;
 
 public class PlanetProperties {
-    private String name;
-    private String group;
-    private int cost;
-    private ArrayList<Double> rentValues;
-    private int position;
+    private final String name;
+    private final String group;
+    private final int cost;
+    private final ArrayList<Double> rentValues;
+    private final int position;
     private boolean mortgaged;
     private Player owner;
-    private boolean canBePurchased;
+    private final boolean canBePurchased;
     private int numberOfUpgrades;
     private int perfectPlanet;
     private boolean upgradePurchasedInCurrentTurn;
-    private int upgradeIndex;
+    private final int upgradeIndex;
 
     public PlanetProperties(String name, String group, int cost,
                             ArrayList<Double> rentValues, int position, boolean canBePurchased,
@@ -31,40 +31,20 @@ public class PlanetProperties {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getGroup() {
         return group;
-    }
-
-    public void setGroup(String group) {
-        this.group = group;
     }
 
     public int getCost() {
         return cost;
     }
 
-    public void setCost(int cost) {
-        this.cost = cost;
-    }
-
     public ArrayList<Double> getRentValues() {
         return rentValues;
     }
 
-    public void setRentValues(ArrayList<Double> rentValues) {
-        this.rentValues = rentValues;
-    }
-
     public int getPosition() {
         return position;
-    }
-
-    public void setPosition(int position) {
-        this.position = position;
     }
 
     public boolean isMortgaged() {
@@ -97,10 +77,6 @@ public class PlanetProperties {
         return canBePurchased;
     }
 
-    public void setCanBePurchased(boolean canBePurchased) {
-        this.canBePurchased = canBePurchased;
-    }
-
     public int getNumberOfUpgrades() {
         return numberOfUpgrades;
     }
@@ -117,20 +93,8 @@ public class PlanetProperties {
         this.perfectPlanet += perfectPlanet;
     }
 
-    public boolean isUpgradePurchasedInCurrentTurn() {
-        return upgradePurchasedInCurrentTurn;
-    }
-
-    public void setUpgradePurchasedInCurrentTurn(
-            boolean upgradePurchasedInCurrentTurn) {
-        this.upgradePurchasedInCurrentTurn = upgradePurchasedInCurrentTurn;
-    }
-
     public int getUpgradeIndex() {
         return upgradeIndex;
     }
 
-    public void setUpgradeIndex(int upgradeIndex) {
-        this.upgradeIndex = upgradeIndex;
-    }
 }
